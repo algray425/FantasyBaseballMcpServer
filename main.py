e9b0ae91caed8ac799c88fe857bdacc0fd4753c1
algray425 <16615934+algray425@users.noreply.github.com>
2026-03-09 15:42:57 -0700

    Add tool for hitter projections
    Adding MCP tool to contact hitter projections endpoint

--- main.py
+++ main.py
@@ -3,13 +3,24 @@ from mcp.server.fastmcp import FastMCP
 import httpx
 
 from typing import Any
+from enum import Enum
 
 mcp = FastMCP("Fantasy Baseball")
 
-HITTER_RANKING_ENDPOINT = "http://localhost:9292/api/v2/players/hitting/stats"
+HITTER_PROJECTIONS_ENDPOINT = "http://localhost:9292/api/v2/players/hitting/projections"
+HITTER_RANKING_ENDPOINT     = "http://localhost:9292/api/v2/players/hitting/stats"
 
-async def makeHitterRankingsRequest(url: str) -> dict[str, Any] | None:
-    """ Make a request to the fantasy baseball API to obtain hitter projections with proper error handling"""
+class HittingStat(Enum):
+    PERCENTILE_OVERALL  = 1
+    RUNS                = 2
+    HOME_RUNS           = 3
+    RBIS                = 4
+    STOLEN_BASES        = 5
+    OBP                 = 6
+    NONE                = 7
+
+async def makeHitterRequest(url: str) -> dict[str, Any] | None:
+    """ Make a request to the fantasy baseball API with proper error handling"""
     async with httpx.AsyncClient() as client:
         try:
             response = await client.get(url, timeout=30.0)
@@ -19,6 +30,29 @@ async def makeHitterRankingsRequest(url: str) -> dict[str, Any] | None:
             return response.json()
         except Exception:
             return None
+
+def formatHitterProjections(hitterProjection: dict) -> str:
+    return f"""
+    Name: {hitterProjection.get("firstName", "Unknown")} {hitterProjection.get("lastName", "Unknown")}
+    Team: {hitterProjection.get("team", "Unknown")}
+    Position: {hitterProjection.get("position", "Unknown")}
+    Runs: {hitterProjection.get("runs", "Unknown")}
+    Home Runs: {hitterProjection.get("homeRuns", "Unknown")}
+    Rbis: {hitterProjection.get("rbis", "Unknown")}
+    Stolen Bases: {hitterProjection.get("stolenBases", "Unknown")}
+    Overall Percentile Runs: {hitterProjection.get("overallPercentileRuns", "Unknown")}
+    Overall Percentile Home Runs: {hitterProjection.get("overallPercentileHomeRuns", "Unknown")}
+    Overall Percentile Rbis: {hitterProjection.get("overallPercentileRbis", "Unknown")}
+    Overall Percentile Stolen Bases: {hitterProjection.get("overallPercentileStolenBases", "Unknown")}
+    Overall Percentile OBP: {hitterProjection.get("overallPercentileOnBasePercentage", "Unknown")}
+    Overall Grade Percentile: {hitterProjection.get("overallPercentileGrade", "Unknown")}
+    Qualified Percentile Runs: {hitterProjection.get("qualifiedPercentileRuns", "Unknown")}
+    Qualified Percentile Home Runs: {hitterProjection.get("qualifiedPercentileHomeRuns", "Unknown")}
+    Qualified Percentile Rbis: {hitterProjection.get("qualifiedPercentileRbis", "Unknown")}
+    Qualified Percentile Stolen Bases: {hitterProjection.get("qualifiedPercentileStolenBases", "Unknown")}
+    Qualified Percentile OBP: {hitterProjection.get("qualifiedPercentileOnBasePercentage", "Unknown")}
+    Qualified Grade Percentile: {hitterProjection.get("qualifiedPercentileGrade", "Unknown")}
+    """
 
 def formatHitterRanking(hitterProjection: dict) -> str:
     return f"""
@@ -33,7 +67,31 @@ def formatHitterRanking(hitterProjection: dict) -> str:
     """
 
 @mcp.tool()
-async def getHitterRankings(season: str, position: str = "", startDate: str = "", endDate: str = "") -> str:
+async def getHitterProjections(position: str = "", qualified: bool = False, sortBy: HittingStat = HittingStat.NONE) -> str:
+    """Get hitter projections for the upcoming season
+
+    Args:
+        position: The position to filter by (e.g. 1B, OF)
+        qualified: Flag to determine if we should filter by qualified hitters or not
+        sortBy: The hitting stat to sort results by (e.g. runs, home runs)
+    """
+
+    url = f"{HITTER_PROJECTIONS_ENDPOINT}?qualified={qualified}"
+
+    if len(position) != 0:
+        url = url + f"&position={position}"
+
+    if sortBy != HittingStat.NONE:
+        url = url + f"&sortBy={sortBy.name}"
+
+    data = await makeHitterRequest(url)
+
+    projections = [formatHitterProjections(hitter) for hitter in data]
+
+    return "\n---\n".join(projections)
+
+@mcp.tool()
+async def getHitterRankings(season: str, position: str = "", startDate: str = "", endDate: str = "", leagueTypeFilter: str = "", leagueIdFilter: str = "") -> str:
     """Get hitter rankings for a season.
 
     Args:
@@ -41,6 +99,8 @@ async def getHitterRankings(season: str, position: str = "", startDate: str = ""
         position: The position to filter by (e.g. 1B, OF)
         startDate: The start date to filter hitter ranks by with format YYYY-MM-DD
         endDate: The end date to filter hitter ranks by with format YYYY-MM-DD
+        leagueTypeFilter: The type of league to filter by (e.g. ESPN)
+        leagueIdFilter: The id of the league to filter by
     """
 
     url = f"{HITTER_RANKING_ENDPOINT}/{season}"
@@ -57,7 +117,13 @@ async def getHitterRankings(season: str, position: str = "", startDate: str = ""
     if len(endDate) != 0:
         url = url + f"endDate={endDate}&"
 
-    data = await makeHitterRankingsRequest(url)
+    if len(leagueTypeFilter) != 0:
+        url = url + f"leagueTypeFilter={leagueTypeFilter}&"
+
+    if len(leagueIdFilter) != 0:
+        url = url + f"leagueIdFilter={leagueIdFilter}&"
+
+    data = await makeHitterRequest(url)
 
     if not data or len(data) == 0:
         return "Unable to fetch hitter rankings for given season"
